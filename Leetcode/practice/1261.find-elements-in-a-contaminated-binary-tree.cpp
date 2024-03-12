4d1a8178d95ca7d5ba3155558bad0eaafa9b7987
bayyy <475417309@qq.com>
2024-03-12 15:59:52 +0800

    📐 Leetcode-Daily
    1261.在受污染的二叉树中查找元素

---
+++ Leetcode/practice/1261.find-elements-in-a-contaminated-binary-tree.cpp
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=1261 lang=cpp
+ * @lcpr version=30119
+ *
+ * [1261] 在受污染的二叉树中查找元素
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class FindElements {
+ private:
+  set<int> s;
+  TreeNode *root;
+  void dfs(TreeNode *node, int val) {
+    if (node == nullptr) return;
+    node->val = val;
+    s.insert(val);
+    dfs(node->left, 2 * val + 1);
+    dfs(node->right, 2 * val + 2);
+  }
+
+ public:
+  FindElements(TreeNode *root) {
+    this->root = root;
+    dfs(root, 0);
+  }
+
+  bool find(int target) { return s.count(target) > 0; }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
+// @lc code=end
+
+/*
+// @lcpr case=start
+// ["FindElements","find","find"][[[-1,null,-1]],[1],[2]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// ["FindElements","find","find","find"][[[-1,-1,-1,-1,-1]],[1],[3],[5]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+//
+["FindElements","find","find","find","find"][[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]\n
+// @lcpr case=end
+
+ */
